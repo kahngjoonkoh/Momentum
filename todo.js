e646178d3f6d7e75b789b4f5e204c5f0d8a0da02
Kahngjoon Koh <46638829+kahngjoonkoh@users.noreply.github.com>
2020-09-14 19:13:36 +0400

    Update on CSS

--- todo.js
+++ todo.js
@@ -26,11 +26,11 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = Date.now();
-  delBtn.innerHTML = "❌";
+  delBtn.innerHTML = "✖️";
   delBtn.addEventListener("click", deleteTodDo);
   span.innerText = text;
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
